5c3522f521c8eae2ca00aa5c9ba3d8a6fc861a77
Lucas Nabesima <lucas.nabesima@gmail.com>
2025-11-14 18:07:40 -0300

    feat: implement 'validAnagram' solution in C#

--- CsharpSolutions/Leetcode.Solutions/Program.cs
+++ CsharpSolutions/Leetcode.Solutions/Program.cs
@@ -2,24 +2,40 @@
 
 public class Program
 {
-    public static bool HasDuplicates(int[] numArr)
+    public static bool IsAnagram(string s, string t)
     {
-        HashSet<int> numSet = new();
-        foreach (int num in numArr)
+        // check if both strings have the same character count
+        // if they don't, they couldn't even be an anagram
+        if(s.Length != t.Length) return false;
+
+        var charMap = new Dictionary<char, int>();
+        
+        // count every letter in string `s`
+        foreach (var letter in s)
         {
-            if (!numSet.Add(num))
-            {
-                return true;
-            }
+            charMap[letter] = charMap.GetValueOrDefault(letter) + 1;
         }
 
-        return false;
+        // try to remove every letter in string `t`
+        // if the letter doesn't exist in the map, or it's count's 0, return false
+        // bc it's not an anagram
+        foreach (var letter in t)
+        {
+            if (!charMap.TryGetValue(letter, out int count) || count == 0)
+            {
+                return false;
+            }
+
+            charMap[letter]--;
+        }
+        // if all letters are removed, then it's an anagram; we should return `true`
+        return true;
     }
     
     static void Main(string[] args)
     {
-        int[] myArr = [1, 2, 3, 3];
-        Console.WriteLine(HasDuplicates(myArr));
-    
+        var string1 = "racecar";
+        var string2 = "carrace";
+        Console.WriteLine(IsAnagram(string1, string2));
     }
 }

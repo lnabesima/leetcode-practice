963241f80d5344644446adba839aed776f531f43
Lucas Nabesima <lucas.nabesima@gmail.com>
2025-11-14 10:59:31 -0300

    feat: add solution in C# using HashSet

---
+++ CsharpSolutions/Leetcode.Solutions/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Leetcode.Solutions;
+
+class Program
+{
+    static bool HasDuplicates(int[] numArr)
+    {
+        HashSet<int> numSet = new();
+        foreach (int num in numArr)
+        {
+            if (!numSet.Add(num))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+    
+    static void Main(string[] args)
+    {
+        int[] myArr = [1, 2, 3, 3];
+        Console.WriteLine(HasDuplicates(myArr));
+    
+    }
+}

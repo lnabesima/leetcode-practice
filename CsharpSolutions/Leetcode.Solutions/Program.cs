99d3dc1efc14cbe17cb49867889d498677df95d8
Lucas Nabesima <lucas.nabesima@gmail.com>
2025-11-17 11:32:02 -0300

    test: add test suite for the C# solution

--- CsharpSolutions/Leetcode.Solutions/Program.cs
+++ CsharpSolutions/Leetcode.Solutions/Program.cs
@@ -1,16 +1,22 @@
-﻿namespace Leetcode.Solutions;
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Leetcode.Solutions;
 
 public class Program
 {
-    public static bool IsAnagram(string s, string t)
+    public static bool IsAnagram(string? s, string? t)
     {
-        // check if both strings have the same character count
-        // if they don't, they couldn't even be an anagram
-        if(s.Length != t.Length) return false;
+        // time complexity: O(n)
+        // space complexity: O(n)
 
-        var charMap = new Dictionary<char, int>();
-        
+        // check if both strings have the same character count or are null
+        // if they don't (or are null), they couldn't even be an anagram
+        if (s == null || t == null) return false;
+        if (s.Length != t.Length) return false;
+
+
         // count every letter in string `s`
+        Dictionary<char, int> charMap = new();
         foreach (var letter in s)
         {
             charMap[letter] = charMap.GetValueOrDefault(letter) + 1;
@@ -28,10 +34,12 @@ public class Program
 
             charMap[letter]--;
         }
+
         // if all letters are removed, then it's an anagram; we should return `true`
         return true;
     }
-    
+
+    [ExcludeFromCodeCoverage]
     static void Main(string[] args)
     {
         var string1 = "racecar";

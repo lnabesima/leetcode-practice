d8d6013e703acbe48470e20a147162bc82f9af36
Lucas Nabesima <lucas.nabesima@gmail.com>
2025-11-18 09:07:02 -0300

    feat: implement 'Valid Anagram' solution in TS

--- TypeScriptSolutions/src/solution.ts
+++ TypeScriptSolutions/src/solution.ts
@@ -1,14 +1,28 @@
-export function hasDuplicates(numArr: number[]): boolean {
-  const numSet = new Set();
+export function isAnagram(s: string, t: string): boolean {
+  // time complexity: O(n)
+  // space complexity: O(n)
 
-  for (const num of numArr) {
-    if (numSet.has(num)) {
-      return true;
-    }
-    numSet.add(num);
+  // if the strings have different character counts, they can't be anagrams
+  if (s === null || t === null) return false;
+  if (s.length !== t.length) return false;
+
+  const charMap = new Map<string, number>();
+  for (const letter of s) {
+    const currentValue = charMap.get(letter) || 0;
+    charMap.set(letter, currentValue + 1);
   }
-  return false;
+
+  for (const letter of t) {
+    const currentValue = charMap.get(letter) || 0;
+
+    // if the current value of letter is 0, then s and t can't be an anagrams
+    if (currentValue === 0) return false;
+
+    charMap.set(letter, currentValue - 1);
+  }
+
+  return true;
 }
 
-const myArr = [1, 2, 3, 3];
-console.log(hasDuplicates(myArr));
+console.log(isAnagram('racecar', 'carrace'));
+console.log(isAnagram('jam', 'jar'));
